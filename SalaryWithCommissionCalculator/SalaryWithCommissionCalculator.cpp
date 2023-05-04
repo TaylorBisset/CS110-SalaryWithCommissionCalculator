88e7aaac197161160a48812e1dd531402bad3708
Taylor Bisset <taylorbisset@gmail.com>
2023-05-04 11:42:00 -0700

    Fixed variables. Created cin and cout

--- SalaryWithCommissionCalculator/SalaryWithCommissionCalculator.cpp
+++ SalaryWithCommissionCalculator/SalaryWithCommissionCalculator.cpp
@@ -9,17 +9,65 @@ using namespace std;
 int main()
 {
 	double baseSalary = 3600;
-	double commissionRate = 0.05;	// 5% commission
-	double salesQuota = 17000;
+	double commissionPercent = 5;					 // 5% commission
+	double commissionRate = commissionPercent / 100; // 5% commission
 	double totalSales = 23800;
-	double commissionPay = 0;
-	if (totalSales > salesQuota) {
-		commissionPay = totalSales - salesQuota;
+	double salesQuota = 17000;
+	double salesOverQuota = 0;
+	if (totalSales > salesQuota)
+	{
+		salesOverQuota = totalSales - salesQuota;
 	}
+	double commissionPay = salesOverQuota * commissionRate;
 	double grossPay = baseSalary + commissionPay;
-	double FICA = grossPay * 0.0765;			// FICA witholding = 7.65% of Gross Pay
-	double incomeTax = grossPay * 0.25;		// Federal income tax = 25% of Gross Pay
+	double FICA = grossPay * 0.0765;				 // FICA witholding = 7.65% of Gross Pay
+	double incomeTax = grossPay * 0.25;				 // Federal income tax = 25% of Gross Pay
 	double netPay = (grossPay - FICA - incomeTax);
+
+	cout << "For the follwing, please enter monthly data points...\n\n"
+		"Please enter your Commission Percentage: ";
+	cin >> commissionPercent;
+	cout << "Please enter your Base Salary: $";
+	cin >> baseSalary;
+	cout << "Please enter your Total Sales: $";
+	cin >> totalSales;
+	cout << "Please enter your Sales Quota: $";
+	cin >> salesQuota;
+
+	cout << setprecision(2) << fixed << endl << endl;
+
+	cout << left << setw(25) << "Total Sales";
+	cout << right << "$" << setw(10) << totalSales << endl;
+	
+	cout << left << setw(25) << "Sales Quota";
+	cout << right << "$" << setw(10) << salesQuota << endl << endl;
+
+	cout << left << setw(25) << "Sales Over Quota";
+	cout << right << "$" << setw(10) << salesOverQuota << endl;
+
+	cout << left << setw(25) << "Commission Percent";
+	cout << right << "%" << setw(10) << commissionPercent << endl;
+
+	cout << left << setw(25) << "Commission Rate";
+	cout << right << " " << setw(10) << commissionRate << endl << endl;
+
+	cout << left << setw(25) << "Commission Pay";
+	cout << right << "$" << setw(10) << commissionPay << endl;
+
+	cout << left << setw(25) << "Base Salary";
+	cout << right << "$" << setw(10) << baseSalary << endl;
+
+	cout << left << setw(25) << "Gross Pay";
+	cout << right << "$" << setw(10) << grossPay << endl << endl;
+
+	cout << left << setw(25) << "FICA witholdings";
+	cout << right << "$" << setw(10) << FICA << endl;
+
+	cout << left << setw(25) << "Income Tax witholdings";
+	cout << right << "$" << setw(10) << incomeTax << endl << endl;
+
+	cout << left << setw(25) << "Your Net Pay";
+	cout << right << "$" << setw(10) << netPay << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
