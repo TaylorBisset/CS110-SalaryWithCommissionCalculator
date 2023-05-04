14b73075a0edcc7771876ee52b62056aeae1aa5e
Taylor Bisset <taylorbisset@gmail.com>
2023-05-04 11:59:04 -0700

    Reorganized code. inputs and outputs work properly.

--- SalaryWithCommissionCalculator/SalaryWithCommissionCalculator.cpp
+++ SalaryWithCommissionCalculator/SalaryWithCommissionCalculator.cpp
@@ -9,30 +9,39 @@ using namespace std;
 int main()
 {
 	double baseSalary = 3600;
-	double commissionPercent = 5;					 // 5% commission
-	double commissionRate = commissionPercent / 100; // 5% commission
+	double commissionRate = 0.05; // 5% commission
 	double totalSales = 23800;
 	double salesQuota = 17000;
 	double salesOverQuota = 0;
+
+	cout << "For the follwing, please enter monthly data points...\n\n";
+
+	cout << "Please enter your Base Salary: $";
+	cin >> baseSalary;
+	
+	cout << "Please enter your Total Sales: $";
+	cin >> totalSales;
+
+	cout << "Please enter your Sales Quota: $";
+	cin >> salesQuota;
+	
+	cout << "Please enter your Commission Rate: ";
+	cin >> commissionRate;
+
+	/*
+	Determine tax witholdings and Net Pay
+	*/
+
 	if (totalSales > salesQuota)
 	{
 		salesOverQuota = totalSales - salesQuota;
 	}
 	double commissionPay = salesOverQuota * commissionRate;
 	double grossPay = baseSalary + commissionPay;
-	double FICA = grossPay * 0.0765;				 // FICA witholding = 7.65% of Gross Pay
-	double incomeTax = grossPay * 0.25;				 // Federal income tax = 25% of Gross Pay
+	double FICA = grossPay * 0.0765;	// FICA witholding = 7.65% of Gross Pay
+	double incomeTax = grossPay * 0.25;	// Federal income tax = 25% of Gross Pay
 	double netPay = (grossPay - FICA - incomeTax);
-
-	cout << "For the follwing, please enter monthly data points...\n\n"
-		"Please enter your Commission Percentage: ";
-	cin >> commissionPercent;
-	cout << "Please enter your Base Salary: $";
-	cin >> baseSalary;
-	cout << "Please enter your Total Sales: $";
-	cin >> totalSales;
-	cout << "Please enter your Sales Quota: $";
-	cin >> salesQuota;
+	
 
 	cout << setprecision(2) << fixed << endl << endl;
 
@@ -44,9 +53,6 @@ int main()
 
 	cout << left << setw(25) << "Sales Over Quota";
 	cout << right << "$" << setw(10) << salesOverQuota << endl;
-
-	cout << left << setw(25) << "Commission Percent";
-	cout << right << "%" << setw(10) << commissionPercent << endl;
 
 	cout << left << setw(25) << "Commission Rate";
 	cout << right << " " << setw(10) << commissionRate << endl << endl;
